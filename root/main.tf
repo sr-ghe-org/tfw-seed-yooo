dcb72025d0abda1ab372bc408fd5579685cc6868
SWETA_RAJ <swetaraj@google.com>
2024-09-26 22:42:11 +0530

    update version

--- root/main.tf
+++ root/main.tf
@@ -1,6 +1,6 @@
 module "vcsworkflow" {
   source  = "app.terraform.io/tfc-test-pool/wrkflw/vcs"
-  version = "0.0.3"
+  version = "0.0.4"
   roles = {
     repository = ["roles/artifactregistry.writer"]
     workspace  = ["roles/iam.workloadIdentityPoolAdmin", "roles/compute.admin"]

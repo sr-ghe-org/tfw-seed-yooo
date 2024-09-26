afdff8010ef881ab31f83590a0c2902d4daf0922
SWETA_RAJ <swetaraj@google.com>
2024-09-26 22:27:46 +0530

    test

--- root/main.tf
+++ root/main.tf
@@ -1,1 +1,75 @@
-# placeholder
+module "vcsworkflow" {
+  source  = "app.terraform.io/tfc-test-pool/wrkflw/vcs"
+  version = "0.0.2"
+  roles = {
+    repository = ["roles/artifactregistry.writer"]
+    workspace  = ["roles/iam.workloadIdentityPoolAdmin", "roles/compute.admin"]
+  }
+  gcp_projects        = { pr = "pryooo", np = "npyooo" }
+  repository_name     = "vcs-qqq"
+  repository_template = "tfw-seed-template"
+  vcs_ghain           = "ghain-9NW7c1EhRWxYsYs5"
+  tfe_project_id      = "prj-BL1UPf9uoMi9mrDG"
+  # tfe_team_id         = ""
+  wif = {
+    repository = {
+      gcp = {
+        pr = {
+          project_number  = "349260731752"
+          pool_id         = "prpoolyooo"
+          provider_id     = "gh-provider"
+        },
+        np = {
+          project_number  = "349260731752"
+          pool_id         = "nppoolyooo"
+          provider_id     = "gh-provider"
+        }
+      },
+    #   hve = {
+    #     address   = "blah.bloo.blee"
+    #     namespace = "doesnotexist"
+    #     auth_path = "/foo/fee/fum"
+    #   }
+    },
+    workspace = {
+      gcp = {
+        pr = {
+          project_number  = "349260731752"
+          pool_id         = "prpoolyooo"
+          provider_id     = "tf-provider"
+        },
+        np = {
+          project_number  = "349260731752"
+          pool_id         = "nppoolyooo"
+          provider_id     = "tf-provider"
+        }
+      },
+    #   hve = {
+    #     pr = {
+    #       address   = "blah.bloo.blee"
+    #       namespace = "doesnotexist"
+    #       auth_path = "/foo/fee/fum"
+    #     },
+    #     np = {
+    #       address   = "blah.bloo.blee"
+    #       namespace = "doesnotexist"
+    #       auth_path = "/foo/fee/fum"
+    #     }
+    #   }
+    }
+  }
+  workload_context = {
+    pr = {
+      prd = {
+        gcp = ["northamerica-northeast2", "northamerica-northeast1", "us-central4"]
+      }
+    },
+    np = {
+      stg = {
+        gcp = ["northamerica-northeast2"]
+      }
+    }
+  }
+  workload_id = "sqqq"
+  workflow_id = "dep"
+}
